4f5d6c4744e94de35a9534bef6aee1d6a9a3affb
Junio Henrique <juunio.henriquee@gmail.com>
2018-04-11 16:42:47 -0300

    test

--- js/main.js
+++ js/main.js
@@ -1,37 +1,12 @@
-let calculateMonthlyPayment = (principal, years, rate) => {
-    let monthlyRate = 0;
-    if (rate) {
-        monthlyRate = rate / 100 / 12;
-    }
-    let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
-    return {principal, years, rate, monthlyPayment, monthlyRate};
-};
+import * as mortgage from './mortgage';
 
-let calculateAmortization = (principal, years, rate) => {
-    let {monthlyRate, monthlyPayment} = calculateMonthlyPayment(principal, years, rate);
-    let balance = principal;
-    let amortization = [];
-    for (let y=0; y<years; y++) {
-        let interestY = 0; //Interest payment for year Y
-        let principalY = 0; //Principal payment for year Y
-        for (let m=0; m<12; m++) {
-            let interestM = balance * monthlyRate;
-            let principalM = monthlyPayment - interestM;
-            interestY = interestY + interestM;
-            principal = principalY + principalM;
-            balance = balance - principalM;
-        }
-        amortization.push({principalY, interestY, balance})
-    }
-    return {monthlyPayment, monthlyRate, amortization};
-};
 
 document.getElementById('calcBtn').addEventListener('click',  () => {
     let principal = document.getElementById("principal").value;
     let years = document.getElementById("years").value;
     let rate = document.getElementById("rate").value;
     // let {monthlyPayment, monthlyRate} = calculateMonthlyPayment(principal, years, rate);
-    let {monthlyPayment, monthlyRate, amortization} = calculateAmortization(principal, years, rate);
+    let {monthlyPayment, monthlyRate, amortization} = mortgage.calculateAmortization(principal, years, rate);
     document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
     document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
     amortization.forEach(month => console.log(month));
